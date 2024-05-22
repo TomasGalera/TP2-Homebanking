380f14600739e2110d206ffbdbb5174532ee6525
TomasGalera <143727579+TomasGalera@users.noreply.github.com>
2024-05-22 10:10:43 -0300

    Algunos cambios en las clases
    Cambio en la fecha de vencimiento de las tarjetas y las cajas de ahorros arrancan en 0

---
+++ TP2-Homebanking/TP2-Homebanking/classes/Client.js
@@ -0,0 +1,93 @@
+let clientId = 1
+
+class Client {
+    constructor(dni, password, clientName, clientSurname, limit, debitCardNumber, habilitacion){
+        this.id = clientId;
+        this.dni = dni;
+        this.password = password;
+        this.name = clientName;
+        this.surname = clientSurname;
+        this.caPesos = 0
+        this.limiteDescubierto = limit;
+        this.deudaDescubierto = 0
+        
+        let currentDate = new Date();
+        this.cards = [{cardNumber: debitCardNumber, cardExpiration: new Date(currentDate.getFullYear() + 3, currentDate.getMonth(), currentDate.getDate())}];
+        
+        if (habilitacion == "si"){
+            this.habilitacion = true
+            this.caDolares = 0;
+        } else {
+            this.habilitacion = false
+        }
+        clientId++
+    }
+    extraction (amount, CajaAhorro){
+        if (CajaAhorro == "dolares" && this.caDolares>amount && this.habilitacion == true){
+            this.caDolares -= amount;
+            console.log(`Dinero extraido de la caja de ahorro en dolares por un monto de: ${amount} \nSu nuevo saldo es de: ${this.caDolares}`)
+            return true
+        } else if(CajaAhorro == "pesos"){
+            if (this.caPesos>=amount){
+                this.caPesos -= amount;
+                console.log(`Dinero extraido de la caja de ahorro en pesos por un monto de: ${amount} \nSu nuevo saldo es de: ${this.caPesos}`)
+            } else {
+                let extraccion = (this.caPesos - amount)*-1;
+                this.caPesos = 0;
+                if (this.deudaDescubierto + extraccion <= this.limiteDescubierto){
+                    this.deudaDescubierto += extraccion
+                    console.log(`Su saldo es de 0 y su deuda de descubierto es ${this.deudaDescubierto}`)
+                    return true
+                } else {
+                    console.log("No es posible hacer la extracción");
+                    return false
+                }
+            }
+        } else {
+            console.log("La caja de ahorros no existe")
+            return false
+        }
+    }
+    deposit(amount, CajaAhorro){
+        if (CajaAhorro == "dolares" && this.habilitacion == true){
+            this.caDolares += amount;
+            console.log(`Dinero depositado en la caja de ahorro en dolares por un monto de: ${amount} \nSu nuevo saldo es de: ${this.caDolares}`)
+            return true
+        } else if(CajaAhorro == "pesos"){
+                this.caPesos += amount;
+                console.log(`Dinero depositado en la caja de ahorro en pesos por un monto de: ${amount} \nSu nuevo saldo es de: ${this.caPesos}`)
+                return true
+        } else {
+            console.log("La caja de ahorros no existe")
+            return false
+        }
+    }
+    cancelarDescubierto(amount){
+        if (amount<=this.caPesos && amount > 0){
+            let deuda = this.deudaDescubierto - amount
+            if (deuda<0){
+                amount += deuda
+                console.log(`Depositaste más que tu deuda, se te debitó solo la parte que debías: ${amount}`)
+            }
+            this.caPesos-=amount;
+            this.deudaDescubierto-=amount
+            console.log(`Descubierto cancelado, su saldo es de: ${this.caPesos}`)
+        } else if (amount>=this.caPesos){
+            console.log("No tenes ese dinero en tu caja de ahorros")
+        } else {
+            console.log("No fue posible completar la operación")
+        }
+    }
+}
+
+
+let clients = [
+    new Client (47944052, "hola", "Tomas", "Galera", 5000, 10000, 47, "04/12", 400, "si"),
+    new Client (47944052, "chau", "Nicolas", "Salles", 500000, 10000000, 87, "04/12", 400, "si"),
+    new Client (47944052, "a", "a", "a", 500000, 10000000, 74, "04/12", 400, "si")
+]
+
+clients[0].extraction(6000, "pesos")
+clients[0].deposit(4000, "pesos")
+clients[0].deposit(4000, "dolares")
+clients[0].cancelarDescubierto(6000)
---
+++ TP2-Homebanking/TP2-Homebanking/functions.js
@@ -0,0 +1,57 @@
+function transferencia(){
+    let senderId = searchClientById(2)
+    let reciverId = searchClientById(1)
+    let amount = document.getElementById("montoTransferencia").value
+    if (clients[senderId].caPesos >= amount && amount > 0) {
+        clients[senderId].caPesos -= amount
+        clients[reciverId].caPesos += amount
+        console.log(`La transferencia se ha realizado con exito, su nuevo saldo es de: ${clients[senderId].caPesos}`)
+    }
+}
+
+function searchClientById(id){
+    let i = 0
+    while (i < clients.length - 1 && id != clients[i].id){
+        i++
+    }
+    if (creditCards[i].id === id){
+        return i
+    } else {
+        return -1
+    }
+}
+
+function searchCreditCard(id){
+    let i = 0
+    while (i < creditCards.length - 1 && id != creditCards[i].id){
+        i++
+    }
+    if (creditCards[i].id === id){
+        return i
+    } else {
+        return -1
+    }
+}
+
+
+function searchConsumption(id){
+    let i = 0
+    while (i < consumptions.length - 1 && consumptions[i].id != id){
+        i++
+    }
+    if (consumptions[i].id === id){
+        return i
+    } else {
+        return -1
+    }
+}
+
+function searchCreditCardByClientId(clientId){
+    let clientCards = []
+    for (let i in creditCards){
+        if (creditCards[i].clientId === clientId){
+            clientCards.push(creditCards[i])
+        }
+    }
+    return clientCards
+}
